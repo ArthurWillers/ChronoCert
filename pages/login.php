9a6ca84c93e48d70e8afabf358514bf09fe5b0cc
willers <arthur2008willers@gmail.com>
2025-05-14 10:40:34 -0300

    Remove inline script for enabling/disabling login button based on input fields

--- pages/login.php
+++ pages/login.php
@@ -63,20 +63,6 @@ require_once '../includes/toast.php';
   </div>
 
   <?php require_once '../includes/bootstrap_script.php' ?>
-  <script>
-    document.addEventListener('DOMContentLoaded', () => {
-      const email_field = document.querySelector('input[name="email_login"]');
-      const password_field = document.querySelector('input[name="password_login"]');
-      const login_btn = document.querySelector('button[name="submit_login"]');
-
-      function toggle_button() {
-        login_btn.disabled = !email_field.value.trim() || !password_field.value.trim();
-      }
-
-      email_field.addEventListener('input', toggle_button);
-      password_field.addEventListener('input', toggle_button);
-    });
-  </script>
   <script src="../assets/js/toast.js"></script>
   <script src="../assets/js/toggle_password_visibility.js"></script>
 </body>
