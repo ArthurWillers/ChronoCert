730ccc0b7718a8a1398f5dd138277009cb3bcef0
willers <arthur2008willers@gmail.com>
2025-07-21 11:13:17 -0300

    refactor: remove public registration and related files for coordinator-only access

--- actions/register.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../includes/session_start.php';
-require_once '../includes/toast.php';
-
-// Public registration is now disabled - only coordinators can register students
-redirect_with_toast('../pages/login.php', 'O cadastro público foi desabilitado. Apenas coordenadores podem cadastrar novos alunos.');
-?>
